90428905b99c6ad333d7c2b6175f29a3080ce1b8
ridhamezrigui <ridhamezrigui07@gmail.com>
2023-03-04 19:09:23 +0100

    integrate conginto and amazon amplify

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -10,44 +10,60 @@ import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 import process from 'process';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure({
+  AWS_PROJECT_REGION: process.env.REACT_APP_AWS_PROJECT_REGION,
+  aws_cognito_identity_pool_id:
+    process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_CLIENT_ID,
+  oauth: {},
+  Auth: {
+    // We are not using an Identity Pool
+    // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
+    region: process.env.REACT_AWS_PROJECT_REGION, // REQUIRED - Amazon Cognito Region
+    userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID, // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID, // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  },
+});
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomeFeedPage />
+    path: '/',
+    element: <HomeFeedPage />,
   },
   {
-    path: "/@:handle",
-    element: <UserFeedPage />
+    path: '/@:handle',
+    element: <UserFeedPage />,
   },
   {
-    path: "/messages",
-    element: <MessageGroupsPage />
+    path: '/messages',
+    element: <MessageGroupsPage />,
   },
   {
-    path: "/messages/@:handle",
-    element: <MessageGroupPage />
+    path: '/messages/@:handle',
+    element: <MessageGroupPage />,
   },
   {
-    path: "/signup",
-    element: <SignupPage />
+    path: '/signup',
+    element: <SignupPage />,
   },
   {
-    path: "/signin",
-    element: <SigninPage />
+    path: '/signin',
+    element: <SigninPage />,
   },
   {
-    path: "/confirm",
-    element: <ConfirmationPage />
+    path: '/confirm',
+    element: <ConfirmationPage />,
   },
   {
-    path: "/forgot",
-    element: <RecoverPage />
-  }
+    path: '/forgot',
+    element: <RecoverPage />,
+  },
 ]);
 
 function App() {
