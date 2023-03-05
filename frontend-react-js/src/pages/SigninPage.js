d81b3f307ceda844e40e72adbca1481956771085
ridhamezrigui <ridhamezrigui07@gmail.com>
2023-03-05 20:39:33 +0100

    continue configure aws amplify

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -1,86 +1,89 @@
 import './SigninPage.css';
-import React from "react";
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
-import { Link } from "react-router-dom";
+import React from 'react';
+import { ReactComponent as Logo } from '../components/svg/logo.svg';
+import { Link } from 'react-router-dom';
+import { Auth } from 'aws-amplify';
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+import Cookies from 'js-cookie';
 
 export default function SigninPage() {
-
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [errors, setErrors] = React.useState('');
 
+  const [cognitoErrors, setCognitoErrors] = React.useState('');
+
   const onsubmit = async (event) => {
+    setCognitoErrors('');
     event.preventDefault();
-    setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
+    try {
+      Auth.signIn(email, password)
+        .then((user) => {
+          localStorage.setItem(
+            'access_token',
+            user.signInUserSession.accessToken.jwtToken
+          );
+          window.location.href = '/';
+        })
+        .catch((err) => {
+          console.log('Error!', err);
+        });
+    } catch (error) {
+      if (error.code == 'UserNotConfirmedException') {
+        window.location.href = '/confirm';
+      }
+      setCognitoErrors(error.message);
     }
-    return false
-  }
+    return false;
+  };
 
   const email_onchange = (event) => {
     setEmail(event.target.value);
-  }
+  };
   const password_onchange = (event) => {
     setPassword(event.target.value);
-  }
+  };
 
   let el_errors;
-  if (errors){
-    el_errors = <div className='errors'>{errors}</div>;
+  if (cognitoErrors) {
+    el_errors = <div className="errors">{cognitoErrors}</div>;
   }
-
   return (
     <article className="signin-article">
-      <div className='signin-info'>
-        <Logo className='logo' />
+      <div className="signin-info">
+        <Logo className="logo" />
       </div>
-      <div className='signin-wrapper'>
-        <form 
-          className='signin_form'
-          onSubmit={onsubmit}
-        >
+      <div className="signin-wrapper">
+        <form className="signin_form" onSubmit={onsubmit}>
           <h2>Sign into your Cruddur account</h2>
-          <div className='fields'>
-            <div className='field text_field username'>
+          <div className="fields">
+            <div className="field text_field username">
               <label>Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={email_onchange} 
-              />
+              <input type="text" value={email} onChange={email_onchange} />
             </div>
-            <div className='field text_field password'>
+            <div className="field text_field password">
               <label>Password</label>
               <input
                 type="password"
                 value={password}
-                onChange={password_onchange} 
+                onChange={password_onchange}
               />
             </div>
           </div>
           {el_errors}
-          <div className='submit'>
-            <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
-            <button type='submit'>Sign In</button>
+          <div className="submit">
+            <Link to="/forgot" className="forgot-link">
+              Forgot Password?
+            </Link>
+            <button type="submit">Sign In</button>
           </div>
-
         </form>
         <div className="dont-have-an-account">
-          <span>
-            Don't have an account?
-          </span>
+          <span>Don't have an account?</span>
           <Link to="/signup">Sign up!</Link>
         </div>
       </div>
-
     </article>
   );
 }
